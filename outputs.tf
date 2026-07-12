6f17407984b5882fb560832d9596943867a1e9ec
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,57 +1,53 @@
-output "app_service_plans" {
-  description = "All app_service_plan resources"
-  value       = azurerm_app_service_plan.app_service_plans
-}
 output "app_service_plans_app_service_environment_id" {
-  description = "List of app_service_environment_id values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.app_service_environment_id]
+  description = "Map of app_service_environment_id values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.app_service_environment_id }
 }
 output "app_service_plans_is_xenon" {
-  description = "List of is_xenon values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.is_xenon]
+  description = "Map of is_xenon values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.is_xenon }
 }
 output "app_service_plans_kind" {
-  description = "List of kind values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.kind]
+  description = "Map of kind values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.kind }
 }
 output "app_service_plans_location" {
-  description = "List of location values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.location]
+  description = "Map of location values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.location }
 }
 output "app_service_plans_maximum_elastic_worker_count" {
-  description = "List of maximum_elastic_worker_count values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.maximum_elastic_worker_count]
+  description = "Map of maximum_elastic_worker_count values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.maximum_elastic_worker_count }
 }
 output "app_service_plans_maximum_number_of_workers" {
-  description = "List of maximum_number_of_workers values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.maximum_number_of_workers]
+  description = "Map of maximum_number_of_workers values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.maximum_number_of_workers }
 }
 output "app_service_plans_name" {
-  description = "List of name values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.name]
+  description = "Map of name values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.name }
 }
 output "app_service_plans_per_site_scaling" {
-  description = "List of per_site_scaling values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.per_site_scaling]
+  description = "Map of per_site_scaling values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.per_site_scaling }
 }
 output "app_service_plans_reserved" {
-  description = "List of reserved values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.reserved]
+  description = "Map of reserved values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.reserved }
 }
 output "app_service_plans_resource_group_name" {
-  description = "List of resource_group_name values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.resource_group_name]
+  description = "Map of resource_group_name values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.resource_group_name }
 }
 output "app_service_plans_sku" {
-  description = "List of sku values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.sku]
+  description = "Map of sku values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.sku }
 }
 output "app_service_plans_tags" {
-  description = "List of tags values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.tags]
+  description = "Map of tags values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.tags }
 }
 output "app_service_plans_zone_redundant" {
-  description = "List of zone_redundant values across all app_service_plans"
-  value       = [for k, v in azurerm_app_service_plan.app_service_plans : v.zone_redundant]
+  description = "Map of zone_redundant values across all app_service_plans, keyed the same as var.app_service_plans"
+  value       = { for k, v in azurerm_app_service_plan.app_service_plans : k => v.zone_redundant }
 }
 
